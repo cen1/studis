6452a5e4058239392d4acdc9ac5f67f758807343
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-04-04 14:16:24 +0200

    Popravljen controller za import. dodajanje v membership in student tabeli

--- studis/Controllers/ImportController.cs
+++ studis/Controllers/ImportController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using studis.Models;
+using System.Web.Security;
 
 namespace studis.Controllers
 {
     public class ImportController : Controller
     {
+        public studisEntities db = new studisEntities();
 
         public ActionResult Import()
         {
@@ -19,68 +21,91 @@ namespace studis.Controllers
         [HttpPost]
         public ActionResult Import(HttpPostedFileBase file)
         {
-            List<string> list = new List<string>();
-            string name = "";
-            string lname = "";
-            string program = "";
-            string mail = "";
-            int line = 1;
-            int counter = 0;
-
-            var filename = Path.GetFileName(file.FileName);
-            var directory = Server.MapPath("~/Content/");
-            var path = Path.Combine(directory, filename);
-            file.SaveAs(path);
-
-            if (file.ContentLength > 0)
+            if (file != null)
             {
-                using (StreamReader sr = new StreamReader(path))
+                List<string> list = new List<string>();
+                string name = "";
+                string lname = "";
+                string program = "";
+                string mail = "";
+                string username = "";
+                int line = 1;
+                int counter = 0;
+                int counterAll = 0;
+
+                var filename = Path.GetFileName(file.FileName);
+                var directory = Server.MapPath("~/Content/");
+                var path = Path.Combine(directory, filename);
+                file.SaveAs(path);
+
+                if (file.ContentLength > 0)
                 {
-                    while (sr.Peek() != -1)
+                    using (StreamReader sr = new StreamReader(path))
                     {
-                        switch (line)
+                        while (sr.Peek() != -1)
                         {
-                            case 1:
-                                name = sr.ReadLine();
-                                line++;
-                                break;
-                            case 2:
-                                lname = sr.ReadLine();
-                                line++;
-                                break;
-                            case 3:
-                                program = sr.ReadLine();
-                                line++;
-                                break;
-                            case 4:
-                                mail = sr.ReadLine();
-                                list.Add(mail);
-                                line++;
+                            switch (line)
+                            {
+                                case 1:
+                                    name = sr.ReadLine();
+                                    line++;
+                                    break;
+                                case 2:
+                                    lname = sr.ReadLine();
+                                    line++;
+                                    break;
+                                case 3:
+                                    program = sr.ReadLine();
+                                    line++;
+                                    break;
+                                case 4:
+                                    mail = sr.ReadLine();
+                                    line++;
+                                    counterAll++;
 
-                                if ((name.Length > 0) && (name.Length <= 30) && (lname.Length > 0) && (lname.Length <= 30) &&
-                                    (program.Length > 0) && (program.Length <= 7) && (mail.Length > 0) && (mail.Length <= 60))
-                                {
-                                    string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-                                    var random = new Random();
-                                    var password = new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+                                    if ((name.Length > 0) && (name.Length <= 30) && (lname.Length > 0) && (lname.Length <= 30) &&
+                                        (program.Length > 0) && (program.Length <= 7) && (mail.Length > 0) && (mail.Length <= 60))
+                                    {
 
-                                    // add user with name,lname,program,mail,password
+                                        string[] tmp = mail.Split('@');
+                                        username = tmp[0];
 
-                                    list.Add(password);
-                                    counter++;
-                                }
+                                        string password = Guid.NewGuid().ToString().Substring(0, 8);
 
-                                break;
-                            case 5:
-                                sr.ReadLine();
-                                line = 1;
-                                break;
+                                        // membership
+                                        MembershipUser user = Membership.CreateUser(username, password, mail);
+                                        MembershipUser myObject = Membership.GetUser(username);
+                                        string userid = myObject.ProviderUserKey.ToString();
+
+                                        // student
+                                        student stud = new student();
+                                        stud.ime = name;
+                                        stud.priimek = lname;
+                                        stud.userId = Convert.ToInt32(userid);
+
+                                        db.students.Add(stud);
+                                        db.SaveChanges();
+                                            
+                                        list.Add("Uporabniško ime: " + username);
+                                        list.Add("Geslo: " + password);
+                                        counter++;
+                                    }
+
+                                    break;
+                                case 5:
+                                    sr.ReadLine();
+                                    line = 1;
+                                    break;
+                            }
                         }
+                        list.Add("Dodanih " + counter + " od " + counterAll);
                     }
                 }
+                TempData["list"] = list;
+                return RedirectToAction("Print");
             }
-            TempData["list"] = list;
-            return RedirectToAction("Print");
+            else
+                return RedirectToAction("Import");
         }
 
         public ActionResult Print()
