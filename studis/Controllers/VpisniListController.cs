3a515829040463941e5a6ad2021bdb710134709f
GitHubGrega <gregor.babnik@me.com>
2015-04-04 18:41:23 +0200

    VpisniListController.cs

---
+++ studis/Controllers/VpisniListController.cs
@@ -0,0 +1,36 @@
+﻿using studis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studis.Controllers
+{
+    public class VpisniListController : Controller
+    {
+        // GET: VpisniList
+        public ActionResult VpisniList()
+        {
+            ViewBag.Title = "VpisniList";
+            ViewBag.StudijskiProgrami = new SelectList(Sifranti.STUDIJSKIPROGRAM);
+            ViewBag.Klasius = Sifranti.KLASIUS;
+            ViewBag.VrstaVpisa = Sifranti.VRSTAVPISA;
+            ViewBag.NacinStudija = Sifranti.NACINSTUDIJA;
+            ViewBag.OblikaStudija = Sifranti.OBLIKASTUDIJA;
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Zajemi(VpisniList vpisniList)
+        {
+            //preveri
+            //shrani
+
+            return View(vpisniList);
+        }
+
+
+    }
+}
