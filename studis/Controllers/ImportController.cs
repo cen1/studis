88ec8f98652ddef20427927326a518124e76c38a
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-04-04 10:26:49 +0200

    Generiranje passwordov, izpis dodanih

--- studis/Controllers/ImportController.cs
+++ studis/Controllers/ImportController.cs
@@ -4,11 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using studis.Models;
 
 namespace studis.Controllers
 {
     public class ImportController : Controller
     {
+
         public ActionResult Import()
         {
             return View();
@@ -17,10 +19,11 @@ namespace studis.Controllers
         [HttpPost]
         public ActionResult Import(HttpPostedFileBase file)
         {
-            string name;
-            string lname;
-            string program;
-            string mail;
+            List<string> list = new List<string>();
+            string name = "";
+            string lname = "";
+            string program = "";
+            string mail = "";
             int line = 1;
             int counter = 0;
 
@@ -51,23 +54,39 @@ namespace studis.Controllers
                                 break;
                             case 4:
                                 mail = sr.ReadLine();
+                                list.Add(mail);
                                 line++;
+
+                                if ((name.Length > 0) && (name.Length <= 30) && (lname.Length > 0) && (lname.Length <= 30) &&
+                                    (program.Length > 0) && (program.Length <= 7) && (mail.Length > 0) && (mail.Length <= 60))
+                                {
+                                    string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+                                    var random = new Random();
+                                    var password = new string(Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+
+                                    // add user with name,lname,program,mail,password
+
+                                    list.Add(password);
+                                    counter++;
+                                }
+
                                 break;
                             case 5:
                                 sr.ReadLine();
                                 line = 1;
                                 break;
                         }
-
-                        //students[counter] =
-                        counter++;
-
-
                     }
                 }
             }
+            TempData["list"] = list;
+            return RedirectToAction("Print");
+        }
 
-            return RedirectToAction("Import", "Import");
+        public ActionResult Print()
+        {
+            ViewBag.List = TempData["list"] as List<string>;
+            return View();
         }
     }
 }
