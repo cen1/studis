02f28809bf756db6e9896cb625c485751bb72b49
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-04-06 17:54:13 +0200

    Popravki za izpis pdf

--- studis/Controllers/ListController.cs
+++ studis/Controllers/ListController.cs
@@ -6,20 +6,73 @@ using System.Web.Mvc;
 using System.IO;
 using iTextSharp.text;
 using MvcRazorToPdf;
+using studis.Models;
+using System.Web.Security;
 
 namespace studis.Controllers
 {
     public class ListController : Controller
     {
-        // GET: Pdf
+        public studisEntities db = new studisEntities();
+
+        //[Authorize(Roles = "Študent")]
+        //[Authorize(Roles = "Referent")]
+        //[Authorize(Roles = "Skrbnik")]
         public ActionResult List()
+        {
+            /*if (Roles.GetRolesForUser().Contains("Skrbnik") || Roles.GetRolesForUser().Contains("Referent"))
+            {
+                return RedirectToAction("PdfSkrbnik");
+            }
+            else if (Roles.GetRolesForUser().Contains("Študent"))
+            {
+                return RedirectToAction("Pdf");
+            }
+
+            return RedirectToAction("Index", "Home");*/
+            return RedirectToAction("PdfSkrbnik");
+        }
+
+        //[Authorize(Roles = "Študent")]
+        //[Authorize(Roles = "Referent")]
+        //[Authorize(Roles = "Skrbnik")]
+        public ActionResult Pdf()
+        {
+/*
+            // preveri če vpisni list obstaja
+            if (vpisnilist) {
+                // da
+                var model = new studis.Models.nekinovga
+                {
+                    student = db.students.SingleOrDefault(m => m.userId == 10),
+                    vpisnilist = db.vpisnilist.SingleOrDefault(m => m.userId == ?)
+                };
+            
+                return View(model);
+            }
+  
+            // ne
+            else
+            {
+                return RedirectToAction("ListEmpty");
+            }
+*/
+            return new PdfActionResult(db.students.SingleOrDefault(m => m.userId == 10));
+        }
+
+        //[Authorize(Roles = "Referent")]
+        //[Authorize(Roles = "Skrbnik")]
+        public ActionResult PdfSkrbnik()
         {
             return View();
         }
 
-        public ActionResult Pdf()
+        //[Authorize(Roles = "Študent")]
+        //[Authorize(Roles = "Referent")]
+        //[Authorize(Roles = "Skrbnik")]
+        public ActionResult ListEmpty()
         {
-            return new PdfActionResult(null);
+            return View();
         }
     }
 }
