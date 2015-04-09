b91097317d0ba2c9912fddde496c56bea60ab03d
domc <domc987@gmail.com>
2015-04-09 21:43:32 +0200

    popravljeno iskanje studentov in vpisnihlistov

--- studis/Scripts/_studentSearch.js
+++ studis/Scripts/_studentSearch.js
@@ -17,4 +17,21 @@
             }
         });
     });
+    $('#SearchString1').keyup(function (e) {
+        var url = "/Student/StudentSearchPDFPartial";
+        var data = $('#SearchString1').val();
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: { 'searchString1': data },
+            success: function (resp) {
+                $('#searchDiv').html(resp);
+                $("#searchDiv  p:nth-child(1)").remove();
+            },
+            error: function (resp) {
+                alert("err with ajax post");
+            }
+        });
+    });
 });
