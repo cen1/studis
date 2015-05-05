c7d16d2d7eb60cd7a6c20fdf20fc30079151cd4c
cen <cen.is.imba@gmail.com>
2015-05-05 16:36:49 +0200

    asd

---
+++ studis/Scripts/brisanje_opozorilo.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#izbrisi").click(function () {
+        console.log("klik");
+        var url = "/IzpitniRok/obstajajoPrijave/" + $("#id option:selected").val();
+        console.log("URL: " + url);
+        alert("asd");
+        $.get(url, function (response) {
+            console.log("noter");
+            var obstajajoPrijave = $.parseJSON(response);
+            console.log(obstajajoPrijave);
+            if (obstajajoPrijave) confirm("Izpini rok že vsebuje prijave");
+        });
+        alert("asd");
+    });
+});
