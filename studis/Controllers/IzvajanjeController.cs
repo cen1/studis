6bbcf698c857193a17290afbe8f25507179f4a65
cen <cen.is.imba@gmail.com>
2015-05-06 14:23:11 +0200

    tmp

--- studis/Controllers/IzvajanjeController.cs
+++ studis/Controllers/IzvajanjeController.cs
@@ -47,6 +47,41 @@ namespace studis.Controllers
         [HttpPost]
         public ActionResult Dodaj(IzvajanjeModel model)
         {
+            List<predmet> listPredmetov = db.predmets.OrderBy(a => a.ime).ToList();
+
+            List<SelectListItem> predmeti = new List<SelectListItem>();
+            foreach (predmet i in listPredmetov)
+            {
+                SelectListItem p = new SelectListItem();
+                int stIzvajanj = i.izvajanjes.Count;
+                p.Value = i.id.ToString();
+                p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.ime + " (" + i.koda + ") - " + stIzvajanj;
+                predmeti.Add(p);
+            }
+            ViewBag.Predmets = new SelectList(predmeti, "Value", "Text");
+
+            List<profesor> listProfesorjev = db.profesors.OrderBy(a => a.priimek).ToList();
+
+            List<SelectListItem> profesorji = new List<SelectListItem>();
+            foreach (profesor i in listProfesorjev)
+            {
+                SelectListItem p = new SelectListItem();
+                p.Value = i.id.ToString();
+                p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.priimek + " " + i.ime;
+                profesorji.Add(p);
+            }
+            ViewBag.Profesors = new SelectList(profesorji, "Value", "Text");
+
+            foreach (predmet i in listPredmetov)
+            {
+                SelectListItem p = new SelectListItem();
+                int stIzvajanj = i.izvajanjes.Count;
+                p.Value = i.id.ToString();
+                p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.ime + " (" + i.koda + ") - " + stIzvajanj;
+                predmeti.Add(p);
+            }
+            ViewBag.Predmets = new SelectList(predmeti, "Value", "Text");
+
             try
             {
                 izvajanje izvajanje = new izvajanje();
@@ -56,7 +91,7 @@ namespace studis.Controllers
                     izvajanje.profesor1 = db.profesors.SingleOrDefault(p => p.id == model.profesor2);
                 if (model.profesor3 != 0 && model.profesor3 != null)
                     izvajanje.profesor2 = db.profesors.SingleOrDefault(p => p.id == model.profesor3);
-                Debug.WriteLine("Dodano izvajanje");
+                System.Diagnostics.Debug.WriteLine("Dodano izvajanje");
                 db.SaveChanges();
             }
             catch
