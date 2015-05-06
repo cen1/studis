63e4e473f1f730a735006967932952f471029224
GitHubGrega <gregor.babnik@me.com>
2015-05-06 14:08:27 +0200

    update

--- studis/Controllers/IzvajanjeController.cs
+++ studis/Controllers/IzvajanjeController.cs
@@ -57,14 +57,42 @@ namespace studis.Controllers
                     izvajanje.profesor1 = db.profesors.SingleOrDefault(p => p.id == model.profesor2);
                 if (model.profesor3 != 0 && model.profesor3 != null)
                     izvajanje.profesor2 = db.profesors.SingleOrDefault(p => p.id == model.profesor3);
-                Debug.WriteLine("Dodano izvajanje");
+                Debug.WriteLine("Dodano izvajanje1");
+                db.izvajanjes.Add(izvajanje);
                 db.SaveChanges();
+                Debug.WriteLine("Dodano izvajanje2");
             }
             catch
             {
                 return View("Error");
             }
-            return View("Dodaj");
+
+
+            List<predmet> listPredmetov = db.predmets.OrderBy(a => a.ime).ToList();
+
+            List<SelectListItem> predmeti = new List<SelectListItem>();
+            foreach (predmet i in listPredmetov)
+            {
+                SelectListItem p = new SelectListItem();
+                int stIzvajanj = i.izvajanjes.Count;
+                p.Value = i.id.ToString();
+                p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.ime + " (" + i.koda + ") - " + stIzvajanj;
+                predmeti.Add(p);
+            }
+            ViewBag.Predmets = new SelectList(predmeti, "Value", "Text");
+
+            List<profesor> listProfesorjev = db.profesors.OrderBy(a => a.priimek).ToList();
+
+            List<SelectListItem> profesorji = new List<SelectListItem>();
+            foreach (profesor i in listProfesorjev)
+            {
+                SelectListItem p = new SelectListItem();
+                p.Value = i.id.ToString();
+                p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.priimek + " " + i.ime;
+                profesorji.Add(p);
+            }
+            ViewBag.Profesors = new SelectList(profesorji, "Value", "Text");
+            return View();
         }
     }
 }
