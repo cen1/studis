29ea437ebe8952d7486633bb8f9d46b4188b2ce4
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-05-21 21:22:10 +0200

    Mock za kartotečni list

---
+++ studis/Controllers/KartotecniListController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studis.Controllers
+{
+    public class KartotecniListController : Controller
+    {
+        // GET: KartotecniList
+        public ActionResult Izpis()
+        {
+            // napolni in vrni možnosti za dropdown list
+
+
+            return View();
+
+            /*
+             * Redirect za napako *
+             TempData["Napaka"] = "Študent nima vzpostavljenega predmetnika!";
+             return RedirectToAction("Napaka"); 
+             */
+        }
+
+        [HttpPost]
+        public ActionResult Izpis(string Selektor, string Selektor2)
+        {
+            // pripravi in vrni podatke za izpis
+
+            return View();
+        }
+
+        public ActionResult Napaka()
+        {
+            ViewBag.Message = TempData["Napaka"];
+            return View();
+        }
+    }
+}
