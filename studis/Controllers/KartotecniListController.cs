d81fdd77d3fb1ef64ec0049223237efe86e763ba
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-05-23 15:42:51 +0200

    Kartotečni list - zadnje opravljanje

--- studis/Controllers/KartotecniListController.cs
+++ studis/Controllers/KartotecniListController.cs
@@ -14,54 +14,135 @@ namespace studis.Controllers
         public studisEntities db = new studisEntities();
 
         // GET: KartotecniList
-        public ActionResult Izpis()
+        [Authorize(Roles = "Referent, Študent")]
+        public ActionResult Izpis(int? vpisna)
         {
-            // pridobi študenta
-            UserHelper uh = new UserHelper();
-            var student = uh.FindByName(User.Identity.Name).students.FirstOrDefault();
-            
-            try
+            // akcija za referenta
+            if (vpisna != null && User.IsInRole("Referent"))
             {
-                // vrni študentove študijske programe
-                var tmp = db.vpis.Where(v => v.vpisnaStevilka == student.vpisnaStevilka).Select(v => v.studijskiProgram).ToList();
-                var seznam = tmp.Distinct();
-                var items = db.sifrant_studijskiprogram.Where(p => seznam.Contains(p.id));
-                ViewBag.Program = new SelectList(items, "id", "naziv");
+                try
+                {
+                    // vrni študentove študijske programe
+                    var tmp = db.vpis.Where(v => v.vpisnaStevilka == vpisna).Select(v => v.studijskiProgram).ToList();
+                    var seznam = tmp.Distinct();
+                    var items = db.sifrant_studijskiprogram.Where(p => seznam.Contains(p.id));
+                    ViewBag.Program = new SelectList(items, "id", "naziv");
+                    ViewBag.Vpisna = vpisna;
+                }
+                catch
+                {
+                    TempData["Napaka"] = "Študent nima nobenega vpisnega lista!";
+                    return RedirectToAction("Napaka");
+                } 
             }
-            catch
+            else
             {
-                TempData["Napaka"] = "Študent nima nobenega vpisnega lista!";
-                return RedirectToAction("Napaka"); 
+                // pridobi študenta
+                UserHelper uh = new UserHelper();
+                var student = uh.FindByName(User.Identity.Name).students.FirstOrDefault();
+
+                try
+                {
+                    // vrni študentove študijske programe
+                    var tmp = db.vpis.Where(v => v.vpisnaStevilka == student.vpisnaStevilka).Select(v => v.studijskiProgram).ToList();
+                    var seznam = tmp.Distinct();
+                    var items = db.sifrant_studijskiprogram.Where(p => seznam.Contains(p.id));
+                    ViewBag.Program = new SelectList(items, "id", "naziv");
+                    ViewBag.Vpisna = student.vpisnaStevilka;
+                }
+                catch
+                {
+                    TempData["Napaka"] = "Študent nima nobenega vpisnega lista!";
+                    return RedirectToAction("Napaka");
+                }
             }
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult Izpis(sifrant_studijskiprogram model, string polaganja)
+        [Authorize(Roles = "Referent, Študent")]
+        public ActionResult Izpis(sifrant_studijskiprogram model, int vpisna, string polaganja)
         {
+            // samo zadnje polaganje
             if (polaganja == "Zadnje polaganje")
             {
+                // pridobi vse vpisne liste za študenta, kjer je študijski program = selected
+                var vpisi = db.vpis.Where(v => v.vpisnaStevilka == vpisna && v.studijskiProgram == model.id).ToList();
+                ViewBag.Vpisi = vpisi;
 
+                // pridobi izvajanja
+                List<izvajanje> izvajanja = new List<izvajanje>();
+                {
+                    foreach (var vpis in vpisi) 
+                    {
+                        foreach (var izv in vpis.izvajanjes.ToList())
+                        {
+                            izvajanja.Add(izv);
+                        }
+                        
+                    }
+                }
+                ViewBag.Izvajanja = izvajanja;
+
+                // pridobi izpitne roke in vrni izpitni roke, ki so bili (zadnji) opravljani
+                List<izpitnirok> roki = new List<izpitnirok>();
+                {
+                    foreach (var izv in izvajanja)
+                    {
+                        var counter = 0;
+                        foreach (var r in izv.izpitniroks.ToList())
+                        {
+                            if (r.prijavanaizpits.Where(p => p.stanje == 2).ToList() != null)
+                            {
+                                counter++;
+                            }                       
+                        }
+
+                        if (counter > 1)
+                        {
+                            roki.Add(izv.izpitniroks.ToList().Last());
+                        }
+                    }
+                }
+                ViewBag.Roki = roki;
+
+                // pridobi opravljanja izpitov
+                List<prijavanaizpit> prijave = new List<prijavanaizpit>();
+                {
+                    foreach (var v in vpisi)
+                    {
+                        foreach (var p in v.prijavanaizpits.ToList())
+                        {
+                            prijave.Add(p);
+                        }       
+                    }
+                }
+                ViewBag.Prijave = prijave;
+
+                // zadnje opravljanje
+                ViewBag.Izbira = 0;
             } 
+
+            // vsa polaganja
             else
             {
+                // pridobi vse vpisne liste za študenta, kjer je študijski program = selected
 
+                // pridobi izvajanja
+
+                // pridobi vse izpitne roke, ki so bili opravljani
+
+                // pridobi opravljanja izpitov
+
+                // vsa opravljanja
+                ViewBag.Izbira = 1;
             }
-
-            // pridobi vse vpisne liste za študenta, kjer je študijski program = selected
-
-            // pridobi izvajanje
-
-            // pridobi izvajatelja
-
-            // pridobi ocene
-
-            // vrni vse podatke
 
             return View();
         }
 
+        [Authorize(Roles = "Referent, Študent")]
         public ActionResult Napaka()
         {
             ViewBag.Message = TempData["Napaka"];
