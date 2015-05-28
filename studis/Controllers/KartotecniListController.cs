3fdb0d63cef8caa25450a17430f55d21326ddc7b
Nejc Dernovšek <nejc.dernovsek@gmail.com>
2015-05-28 16:42:38 +0200

    Fix za izpisovanje ocen zadnjega polaganja

--- studis/Controllers/KartotecniListController.cs
+++ studis/Controllers/KartotecniListController.cs
@@ -98,18 +98,9 @@ namespace studis.Controllers
                 {
                     foreach (var izv in izvajanja)
                     {
-                        var counter = 0;
-                        foreach (var r in izv.izpitniroks.ToList())
+                        foreach( var r in izv.izpitniroks.OrderByDescending(r => r.datum).ToList())
                         {
-                            if (r.prijavanaizpits.Where(p => p.stanje == 2).ToList() != null)
-                            {
-                                counter++;
-                            }                       
-                        }
-
-                        if (counter > 1)
-                        {
-                            roki.Add(izv.izpitniroks.ToList().Last());
+                            roki.Add(r);
                         }
                     }
                 }
