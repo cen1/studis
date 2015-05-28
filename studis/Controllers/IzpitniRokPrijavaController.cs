9a824844595b072e070d69afefc3dfa9af9878f7
GitHubGrega <gregor.babnik@me.com>
2015-05-28 11:18:52 +0200

    prijava

--- studis/Controllers/IzpitniRokPrijavaController.cs
+++ studis/Controllers/IzpitniRokPrijavaController.cs
@@ -140,7 +140,7 @@ namespace studis.Controllers
                     var profesor3 = i.profesor2;
                     profesorji += ", " + profesor3.ime + " " + profesor3.priimek;
                 }
-
+                //izvajanje.izvajanjeleto.sifrant_studijskoleto.naziv
                 profesorji = profesorji + " (" + i.sifrant_studijskoleto.naziv + ")";
                 seznamIzvajanja.Add(new SelectListItem() { Value = i.id.ToString(), Text = profesorji });
             }
