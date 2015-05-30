fdff5e9e374c92458c56ea3d2fe5d69645b9f718
domc <domc987@gmail.com>
2015-05-30 11:21:44 +0200

    seznamFix

--- studis/Controllers/IzpitniRokController.cs
+++ studis/Controllers/IzpitniRokController.cs
@@ -220,8 +220,10 @@ namespace studis.Controllers
                 p.Text = Convert.ToInt32(p.Value).ToString("000") + " - " + i.ime + " (" + i.koda + ")";
                 predmeti.Add(p);
             }
+
+
             List<SelectListItem> ltemp = new List<SelectListItem>();
-            ltemp.Add(new SelectListItem() { Value = "", Text = "Izbira" });
+            ltemp.Add(new SelectListItem() { Value = "", Text = "Iščem.." });
             ViewBag.Prazen = new SelectList(ltemp, "Value", "Text");
             ViewBag.Predmets = new SelectList(predmeti, "Value", "Text");
             return View();
@@ -1077,6 +1079,79 @@ namespace studis.Controllers
             }
             catch (Exception e) { st = -1; }
             return st.ToString();
+        }
+
+        //dobi izvajanja za predmet brez opcije "Izberi"
+        public string GetIzvajanjaOnlyForPredmet(int id)
+        {
+            /*
+             * TO DO TO DO TO DO TO DO TO DO TO DO
+             */
+            //Debug.WriteLine("ID " + id);
+            int iid = Convert.ToInt32(id);
+            //Debug.WriteLine("ID " + iid);
+            var pPredmet = db.predmets.SingleOrDefault(p => p.id == iid);
+            var izvajanja = pPredmet.izvajanjes.ToList();//izpitniroks.ToList(); //Exception 
+            var seznamIzvajanja = new List<SelectListItem>();
+            int c = 0;
+            foreach (izvajanje i in izvajanja)
+            {
+                c++;
+                string profesorji = "";
+                var profesor1 = i.profesor;//db.profesors.SingleOrDefault( p => p.id == i.izvajalec1Id);
+                profesorji += profesor1.ime + " " + profesor1.priimek;
+                if (i.profesor1 != null)
+                {
+                    var profesor2 = i.profesor1;// db.profesors.SingleOrDefault(p => p.id == i.izvajalec2Id);
+                    profesorji += ", " + profesor2.ime + " " + profesor2.priimek;
+                }
+                if (i.profesor2 != null)
+                {
+                    var profesor3 = i.profesor2;//db.profesors.SingleOrDefault(p => p.id == i.izvajalec3Id);
+                    profesorji += ", " + profesor3.ime + " " + profesor3.priimek;
+                }
+
+                seznamIzvajanja.Add(new SelectListItem() { Value = i.id.ToString(), Text = profesorji });
+            }
+            if (c < 1)
+            {
+                seznamIzvajanja.Add(new SelectListItem() { Value = "", Text = "Ni izvajalcev." });
+            }
+            return new JavaScriptSerializer().Serialize(seznamIzvajanja);
+        }
+
+        //Izpitni roki brez opcije "Izberi"
+        public string GetIzpitniRoksOnlyForIzvajanja(int id)
+        {
+
+            //Debug.WriteLine("ID " + id);
+            int iid = Convert.ToInt32(id);
+            //Debug.WriteLine("ID " + iid);
+            var izvajanje = db.izvajanjes.SingleOrDefault(i => i.id == iid);//db.predmets.SingleOrDefault(p => p.id == iid);
+            var izpitniRoki = izvajanje.izpitniroks;//pPredmet.izpitniroks.ToList(); //Exception 
+            var seznamIzpitniRoki = new List<SelectListItem>();
+            int c = 0;
+            foreach (izpitnirok i in izpitniRoki)
+            {
+                c++;
+                string prostor = "";
+                if (i.sifrant_prostor != null)
+                {
+                    prostor = i.sifrant_prostor.naziv;
+                }
+                string ura = "";
+                if (i.ura != null)
+                {
+                    ura = UserHelper.TimeToString((DateTime)i.ura);
+                }
+                seznamIzpitniRoki.Add(new SelectListItem() { Value = i.id.ToString(), Text = UserHelper.DateToString(i.datum) + " " + ura + " " + prostor });
+            }
+            if (c < 1)
+            {
+                seznamIzpitniRoki.Add(new SelectListItem() { Value = "", Text = "Ta predmet nima razpisanih rokov." });
+            }
+
+            return new JavaScriptSerializer().Serialize(seznamIzpitniRoki);
         }
     }
 }
